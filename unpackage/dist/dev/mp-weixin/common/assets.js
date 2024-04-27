f796dfd14d0e35600d158f734ad800cfe4e7c229
胡一刀 <17355479052@163.com>
2024-04-27 17:20:09 +0800

    feat: update cached file

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const _imports_0 = "/assets/banner1.a7a3b276.jpg";
-const _imports_1 = "/assets/banner2.74187479.jpg";
-const _imports_2 = "/assets/banner3.f3dafc89.jpg";
-exports._imports_0 = _imports_0;
-exports._imports_1 = _imports_1;
-exports._imports_2 = _imports_2;
