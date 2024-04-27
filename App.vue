510b3d27b1867ae984a57e14e4d344687e6670a1
胡一刀 <17355479052@163.com>
2024-04-27 22:25:33 +0800

    feat: 公告swiper

--- App.vue
+++ App.vue
@@ -15,4 +15,8 @@
 <style lang="scss">
 	/*每个页面公共css */
 	@import url("common/style/global.scss");
+	page {
+		padding: 0 30rpx;
+		box-sizing: border-box;
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -13,13 +13,29 @@
 				</swiper-item>
 			</swiper>
 		</view>
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text>公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical circular :autoplay="true" :interval="1500" :duration="300">
+					<swiper-item v-for="item in 3">
+						<view class="swiper-item">测试内容测试内容测试内容测试内容测试内容测试内容测试内容</view>
+					</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="16" color="#333"></uni-icons>
+			</view>
+		</view>
 	</view>
 </template>
 
 <style lang="scss" scoped>
 	.home-layout {
 		.banner {
-			width: 750rpx;
+			width: 100%;
 			padding: 30rpx 0;
 			swiper {
 				width: 100%;
@@ -27,13 +43,53 @@
 					&-item  {
 						width: 100%;
 						height: 100%;
-						padding: 0 30rpx;
 						image {
 							width: 100%;
 							height: 100%;
 							border-radius: 10rpx;
 						}
 					}
+			}
+		}
+		.notice {
+			display: flex;
+			height: 80rpx;
+			background-color: #f9f9f9;
+			border-radius: 80rpx;
+			margin: 0 auto;
+			.left {
+				width: 140rpx;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				> text {
+					color: #28b398;
+					font-weight: 600;
+					font-size: 28rpx;
+				}
+			}
+			.center {
+				flex: 1;
+				swiper {
+					height: 100%;
+					&-item {
+						display: flex;
+						align-items: center;
+						font-size: 30rpx;
+						color: #666;
+						.swiper-item{
+							overflow: hidden;
+							white-space: nowrap;
+							text-overflow: ellipsis;
+						}
+					}
+				}
+			}
+			.right {
+				width: 70rpx;
+				display: flex;
+				justify-content: center;
+				align-items: center;
 			}
 		}
 	}
