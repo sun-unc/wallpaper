a41623cf8c0482d87f9fe0f5b34518d3bb2a5f46
sun-unc <884226421@qq.com>
2024-04-28 21:07:40 +0800

    feat: tabBar及globalStyle设置

---
+++ pages/classify/classify.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <view class="classfy-layout">
+	<view class="classfy" v-for="item in 15">
+		<ThemeItem ></ThemeItem>
+	</view>
+  </view>
+</template>
+
+<style scoped lang="scss">
+	.classfy-layout {
+		padding: 30rpx;
+		display: grid;
+		gap: 15rpx;
+		grid-template-columns: repeat(3, 1fr);
+		.classfy {
+			width: 100%;
+		}
+	}
+</style>
