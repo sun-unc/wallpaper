7aa3a81072e220bec9fb8ed445e3dc5419b9a013
sun-unc <884226421@qq.com>
2024-04-29 17:29:59 +0800

    feat: "我的"页面UI

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,7 +1,131 @@
-<script setup></script>
+<script setup>
+	const sectionInfo = [
+		{
+			icon: "download-filled",
+			text: "我的下载",
+			value: 0
+		},
+		{
+			icon: "star-filled",
+			text: "我的评分",
+			value: 0
+		}
+	]
+	
+	const sectionHelp = [
+		{
+			icon: "chatboxes-filled",
+			text: "联系客服",
+		},
+		{
+			icon: "notification-filled",
+			text: "订阅更新",
+		},
+		{
+			icon: "help-filled",
+			text: "常见问题",
+		}
+	]
+</script>
 
 <template>
-  <div></div>
+  <view class="user-layout">
+	<view class="user-info">
+		<view class="avatar">
+			<image src="../../static/logo.webp" mode="aspectFill"></image>
+		</view>
+		<view class="user-name">
+			Looooo
+		</view>
+	</view>
+	<view class="section">
+		<view class="list">
+			<view class="row" v-for="(item, index) in sectionInfo" :key="index">
+				<view class="left">
+					<uni-icons :type="item.icon" size="20" color="#1E74F0"></uni-icons>
+					<div class="text">{{item.text}}</div>
+				</view>
+				<view class="right">
+					<view class="text">{{item.value}}</view>
+					<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+				</view>
+			</view>
+		</view>
+	</view>
+	<view class="section">
+		<view class="list">
+			<view class="row" v-for="(item, index) in sectionHelp" :key="index">
+				<view class="left">
+					<uni-icons :type="item.icon" size="20" color="#1E74F0"></uni-icons>
+					<div class="text">{{item.text}}</div>
+				</view>
+				<view class="right">
+					<view class="text" v-if="item.value">{{item.value}}</view>
+					<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+				</view>
+			</view>
+		</view>
+	</view>
+  </view>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.user-layout {
+		padding: 0 30rpx;
+		.user-info {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			padding: 50rpx 0;
+			.avatar {
+				width: 160rpx;
+				height: 160rpx;
+				border-radius: 50%;
+				overflow: hidden;
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.user-name {
+				font-size: 44rpx;
+				color: #333;
+				padding: 20rpx 0 5rpx;
+			}
+		}
+		.section {
+			width: 100%;
+			margin: 50rpx auto;
+			border: 1px solid #eee;
+			border-radius: 10rpx;
+			box-shadow: 0 0 30rpx rgba(0,0,0,0.05);
+			.list {
+				.row {
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+						padding: 0 30rpx;
+						height: 100rpx;
+						border-bottom: 1px solid #eee;
+						&:last-child {
+							
+						}
+						.left,.right {
+							display: flex;
+							align-items: center;
+							&.left > .text {
+								padding-left: 20rpx;
+								color: #666;
+							}
+							&.right > .text {
+								font-size: 28rpx;
+								color: #aaa;
+							}
+						}
+				}
+			}
+		}
+	}
+
+</style>
